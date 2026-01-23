6b0085b218dd706c5dbf8670cdf6f6884ba6ca0f
Carolina785 <cfeliciano178@gmail.com>
2026-01-22 21:31:30 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// Classe que representa um herói genérico
 class Heroi{
+    // Método construtor: recebe nome, idade e tipo do herói
     constructor(nome, idade, tipo){
         this.nome = nome
         this.idade = idade
         this.tipo = tipo
     }
-    atacar(){
-        let ataque
 
+    // Método responsável pelo ataque do herói
+    atacar(){
+        let ataque // Variável que irá armazenar o tipo de ataque
+
+        // Estrutura de decisão para definir o ataque conforme o tipo do herói
         if(this.tipo === "mago"){
             ataque = "magia"
         } else if (this.tipo === "guerreiro"){
@@ -17,12 +22,20 @@ class Heroi{
             ataque = "shuriken"
         }
 
+        // Exibe a mensagem final
         console.log(`O ${this.tipo} atacou usando ${ataque}`)
     }
 }
 
-let Heroi1 = new Heroi ("Arthur", 50, "guerreiro")
-Heroi1.atacar()
+// Criação de um herói do tipo guerreiro
+let Heroi1 = new Heroi ("Arcadius", 70, "mago")
+let Heroi2 = new Heroi ("Arthur", 50, "guerreiro")
+let heroi3 = new Heroi ("Valen", 90, "monge")
+let Heroi4 = new Heroi ("Takeda", 45, "ninja")
 
-let Heroi2 = new Heroi ("Takeda", 45, "ninja")
+
+// Chama o método atacar
+Heroi1.atacar()
 Heroi2.atacar()
+heroi3.atacar()
+Heroi4.atacar()
