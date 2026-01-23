c85e135c2c13805d52f6ca8463be023a956cc4b3
Carolina785 <cfeliciano178@gmail.com>
2026-01-22 21:17:23 -0300

    projeto pronto

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-asdfs;
+class Heroi{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+    atacar(){
+        let ataque
+
+        if(this.tipo === "mago"){
+            ataque = "magia"
+        } else if (this.tipo === "guerreiro"){
+            ataque = "espada"
+        } else if (this.tipo === "monge"){
+            ataque = "artes marciais"
+        } else if (this.tipo === "ninja"){
+            ataque = "shuriken"
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
+
+let Heroi1 = new Heroi ("Arthur", 50, "guerreiro")
+Heroi1.atacar()
+
+let Heroi2 = new Heroi ("Takeda", 45, "ninja")
+Heroi2.atacar()
